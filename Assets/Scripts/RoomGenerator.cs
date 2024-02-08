f2313c5d27b9bd885c636b47fe3d082f0750b09d
cf21683 <cf21683@bristol.ac.uk>
2024-02-08 18:13:15 +0000

    add the door prefab and install xr plugin

--- Assets/Scripts/RoomGenerator.cs
+++ Assets/Scripts/RoomGenerator.cs
@@ -6,11 +6,15 @@ public class RoomGenerator : MonoBehaviour
 {
     public GameObject wallPrefab;
     public GameObject floorPrefab;
+
+    public GameObject doorPrefab;
+    
     public Vector3 roomSize = new Vector3(10, 3, 10);
 
     void Start()
     {
         GenerateRoom();
+        GenerateDoor();
     }
 
     void GenerateRoom()
@@ -35,5 +39,22 @@ public class RoomGenerator : MonoBehaviour
 
         GameObject rightWall = Instantiate(wallPrefab, new Vector3(roomSize.x / 2, wallHeight / 2, 0), Quaternion.Euler(0, 90, 0));
         rightWall.transform.localScale = new Vector3(roomSize.z, wallHeight, wallThickness); 
+    }
+
+    void GenerateDoor()
+    {
+        float wallHeight = 3; 
+        float doorWidth = 1; 
+        
+        
+        float minX = -roomSize.x / 2 + doorWidth / 2; 
+        float maxX = roomSize.x / 2 - doorWidth / 2; 
+        float randomX = Random.Range(minX, maxX); 
+
+        
+        Vector3 doorPosition = new Vector3(randomX, wallHeight / 2, -roomSize.z / 2 + 0.2f); 
+        Quaternion doorRotation = Quaternion.Euler(0, 0, 0); 
+        GameObject door = Instantiate(doorPrefab, doorPosition, doorRotation);
+        door.transform.localScale = new Vector3(0.2f,0.2f,0.18f);
     }
 }
