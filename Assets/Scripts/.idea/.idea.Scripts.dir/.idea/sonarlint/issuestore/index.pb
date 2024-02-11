e89bb1a6eba6acc44503eeb24273db7f4b004a3c
cf21683 <cf21683@bristol.ac.uk>
2024-02-11 16:22:08 +0000

    add the script to spawn furniture

--- Assets/Scripts/.idea/.idea.Scripts.dir/.idea/sonarlint/issuestore/index.pb
+++ Assets/Scripts/.idea/.idea.Scripts.dir/.idea/sonarlint/issuestore/index.pb
@@ -1,7 +1,9 @@
 
 @
 RoomGenerator.cs,4/4/447154ee80663464293c88e46655b94b6d1d6165
-D
-FunitureGenerator.cs,3/c/3cf3ee3c034335481f2f0928c864f06caf38f312
 E
 FurnitureGenerator.cs,0/7/07bb680df039ad86b021c6509c3b0721daf65ced
+J
+FurnitureGenerator.cs.meta,0/7/0734e3da38b7d7cd89fb46630d43736084045d26
+E
+RoomGenerator.cs.meta,2/a/2a05b671c36a1a72c26a37569b8f10f75af2a28b
--- Assets/Scripts/FurnitureGenerator.cs
+++ Assets/Scripts/FurnitureGenerator.cs
@@ -1,7 +1,25 @@
-namespace DefaultNamespace;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class FunitureGenerator
+public class FurnitureGenerator : MonoBehaviour
 {
-    public GameObject sofaAndTableAndTvPrefab
-    
+    public GameObject sofaAndTableAndTvPrefab; 
+	
+	public Vector3 roomSize = new Vector3(10, 3, 10);
+	float wallThickness = 0.4f;
+    float wallHeight = 3; 
+    void Start()
+    {
+        GenerateFurniture();
+    }
+
+    void GenerateFurniture()
+    {
+        Vector3 sofaAndTableAndTvPosition = new Vector3(-3.6f, 0.8f, 4.45f);
+        Quaternion sofaAndTableAndTvRotation = Quaternion.Euler(0, -180, 0);
+        GameObject sofaAndTableAndTv =
+            Instantiate(sofaAndTableAndTvPrefab, sofaAndTableAndTvPosition, sofaAndTableAndTvRotation); 
+       sofaAndTableAndTv.transform.localScale = new Vector3(0.5f,0.5f,0.5f);
+    }
 }
