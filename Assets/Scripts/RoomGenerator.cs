56d1df114525765f5e7c4ff8e74e5b56fef8daf8
cf21683 <cf21683@bristol.ac.uk>
2024-02-14 21:03:37 +0000

    update the wall texture， and each wall has random wall texture

--- Assets/Scripts/RoomGenerator.cs
+++ Assets/Scripts/RoomGenerator.cs
@@ -6,8 +6,9 @@ public class RoomGenerator : MonoBehaviour
 {
     public GameObject wallPrefab;
     public GameObject floorPrefab;
-
     public GameObject doorPrefab;
+
+    public Material[] wallMaterials;
     
     public Vector3 roomSize = new Vector3(10, 3, 10);
 
@@ -28,16 +29,20 @@ public class RoomGenerator : MonoBehaviour
         
       
         GameObject frontWall = Instantiate(wallPrefab, new Vector3(0, wallHeight / 2, -roomSize.z / 2), Quaternion.identity);
+        CustomizeWall(frontWall);
         frontWall.transform.localScale = new Vector3(roomSize.x, wallHeight, wallThickness); 
 
         GameObject backWall = Instantiate(wallPrefab, new Vector3(0, wallHeight / 2, roomSize.z / 2), Quaternion.identity);
+        CustomizeWall(backWall);
         backWall.transform.localScale = new Vector3(roomSize.x, wallHeight, wallThickness); 
         
       
         GameObject leftWall = Instantiate(wallPrefab, new Vector3(-roomSize.x / 2, wallHeight / 2, 0), Quaternion.Euler(0, 90, 0));
-        leftWall.transform.localScale = new Vector3(roomSize.z, wallHeight, wallThickness); 
+        CustomizeWall(leftWall);
+        leftWall.transform.localScale = new Vector3(roomSize.z, wallHeight, wallThickness);
 
         GameObject rightWall = Instantiate(wallPrefab, new Vector3(roomSize.x / 2, wallHeight / 2, 0), Quaternion.Euler(0, 90, 0));
+        CustomizeWall(rightWall);
         rightWall.transform.localScale = new Vector3(roomSize.z, wallHeight, wallThickness); 
     }
 
@@ -56,5 +61,18 @@ public class RoomGenerator : MonoBehaviour
         Quaternion doorRotation = Quaternion.Euler(0, 0, 0); 
         GameObject door = Instantiate(doorPrefab, doorPosition, doorRotation);
         door.transform.localScale = new Vector3(0.2f,0.2f,0.18f);
+    }
+
+    void CustomizeWall(GameObject wall)
+    {
+        if (wallMaterials.Length > 0)
+        {
+            int index = Random.Range(0, wallMaterials.Length);
+            Renderer wallRenderer = wall.GetComponent<Renderer>();
+            if (wallRenderer != null)
+            {
+                wallRenderer.material = wallMaterials[index];
+            }
+        }
     }
 }
