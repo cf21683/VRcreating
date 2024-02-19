2ee1ef7c9172433791ccd72f21c308082afd9f66
cf21683 <cf21683@bristol.ac.uk>
2024-02-19 00:31:33 +0000

    The wall will leave a space for the door
     but the model and the door overlap (to be fixed)

--- Assets/Scripts/RoomGenerator.cs
+++ Assets/Scripts/RoomGenerator.cs
@@ -15,56 +15,83 @@ public class RoomGenerator : MonoBehaviour
     void Start()
     {
         GenerateRoom();
-        GenerateDoor();
     }
 
     void GenerateRoom()
     {
-        
         GameObject floor = Instantiate(floorPrefab, transform.position, Quaternion.identity);
         floor.transform.localScale = new Vector3(roomSize.x, 1, roomSize.z);
 
         float wallThickness = 0.4f;
         float wallHeight = 3;
-
+        
+        
+        
+        float doorPositionX = Random.Range(-roomSize.x / 2 + 1.5f, roomSize.x / 2 - 1.5f);
+        Vector3 doorPosition = new Vector3(doorPositionX, wallHeight / 2 + 0.25f, -roomSize.z / 2 + wallThickness / 2);
+        float doorWidth = GenerateDoor(doorPosition);
+        
+        
         int index = Random.Range(0, wallMaterials.Length);
-      
-        GameObject frontWall = Instantiate(wallPrefab, new Vector3(0, wallHeight / 2, -roomSize.z / 2), Quaternion.identity);
-        CustomizeWall(frontWall,index);
-        frontWall.transform.localScale = new Vector3(roomSize.x, wallHeight, wallThickness); 
 
+        
+        GenerateFrontWallSegments(doorPositionX, doorWidth, wallHeight, wallThickness, index);
+        GenerateOtherWalls(wallHeight, wallThickness, index);
+
+      
+        
+        
+    }
+
+    void GenerateFrontWallSegments(float doorPositionX, float doorWidth, float wallHeight, float wallThickness, int index)
+    {
+        float frontLeftWallWidth = doorPositionX + roomSize.x / 2 - doorWidth * 0.25f / 2;
+        float frontRightWallWidth = roomSize.x / 2 - doorPositionX - doorWidth * 0.25f / 2;
+
+       
+      
+            GameObject frontLeftWall = Instantiate(wallPrefab, new Vector3(-roomSize.x / 2 + frontLeftWallWidth / 2, wallHeight / 2, -roomSize.z / 2), Quaternion.identity);
+            CustomizeWall(frontLeftWall, index);
+            frontLeftWall.transform.localScale = new Vector3(frontLeftWallWidth, wallHeight, wallThickness);
+        
+
+        
+        
+            GameObject frontRightWall = Instantiate(wallPrefab, new Vector3(doorPositionX + doorWidth / 2 + frontRightWallWidth / 2, wallHeight / 2, -roomSize.z / 2), Quaternion.identity);
+            CustomizeWall(frontRightWall, index);
+            frontRightWall.transform.localScale = new Vector3(frontRightWallWidth, wallHeight, wallThickness);
+        
+    }
+
+    void GenerateOtherWalls(float wallHeight, float wallThickness, int index)
+    {
+        
         GameObject backWall = Instantiate(wallPrefab, new Vector3(0, wallHeight / 2, roomSize.z / 2), Quaternion.identity);
-        CustomizeWall(backWall,index);
+        CustomizeWall(backWall, index);
         backWall.transform.localScale = new Vector3(roomSize.x, wallHeight, wallThickness); 
         
-      
+       
         GameObject leftWall = Instantiate(wallPrefab, new Vector3(-roomSize.x / 2, wallHeight / 2, 0), Quaternion.Euler(0, 90, 0));
-        CustomizeWall(leftWall,index);
+        CustomizeWall(leftWall, index);
         leftWall.transform.localScale = new Vector3(roomSize.z, wallHeight, wallThickness);
 
+        
         GameObject rightWall = Instantiate(wallPrefab, new Vector3(roomSize.x / 2, wallHeight / 2, 0), Quaternion.Euler(0, 90, 0));
-        CustomizeWall(rightWall,index);
+        CustomizeWall(rightWall, index);
         rightWall.transform.localScale = new Vector3(roomSize.z, wallHeight, wallThickness); 
     }
 
-    void GenerateDoor()
+    float GenerateDoor(Vector3 position)
     {
-        float wallHeight = 3; 
-        float doorWidth = 1; 
-        
-        
-        float minX = -roomSize.x / 2 + doorWidth / 2; 
-        float maxX = roomSize.x / 2 - doorWidth / 2; 
-        float randomX = Random.Range(minX, maxX); 
+        Quaternion doorRotation = Quaternion.identity; 
+        GameObject door = Instantiate(doorPrefab, position, doorRotation);
+        door.transform.localScale = new Vector3(0.25f, 0.25f, 0.1f);
 
-        
-        Vector3 doorPosition = new Vector3(randomX, wallHeight / 2, -roomSize.z / 2 + 0.2f); 
-        Quaternion doorRotation = Quaternion.Euler(0, 0, 0); 
-        GameObject door = Instantiate(doorPrefab, doorPosition, doorRotation);
-        door.transform.localScale = new Vector3(0.2f,0.2f,0.18f);
+        float doorWidth = door.transform.localScale.x * 3f;
+        return doorWidth;
     }
 
-    void CustomizeWall(GameObject wall,int index)
+    void CustomizeWall(GameObject wall, int index)
     {
         if (wallMaterials.Length > 0)
         {
