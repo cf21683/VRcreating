21e2ef9d4d0d90456e247f40b0293fb81f121445
cf21683 <cf21683@bristol.ac.uk>
2024-02-21 18:20:40 +0000

    The left controller can control the left and right rotation of the field of view

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,14 +2,19 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5.0f; 
-
+    public float speed = 2.0f;
+    public float rotationSpeed = 100.0f;
+    
     void Update()
     {
         
         Vector2 input = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
         
         Vector3 moveDirection = new Vector3(input.x, 0, input.y);
+        moveDirection = transform.TransformDirection(moveDirection);
         transform.Translate(moveDirection * speed * Time.deltaTime, Space.World);
+
+        float rotation = input.x * rotationSpeed * Time.deltaTime;
+        transform.Rotate(0, rotation, 0, Space.World);
     }
 }    
