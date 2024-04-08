12e74d55b201c15fb82cca36dbcd479aec30782d
cf21683 <cf21683@bristol.ac.uk>
2024-04-09 02:08:56 +0800

    save play mode change scirpt can save the scale

--- Assets/Editor/SavePlayModeChanges.cs
+++ Assets/Editor/SavePlayModeChanges.cs
@@ -21,12 +21,19 @@ public class SavePlayModeChanges
         Debug.Log($"Saving position for object: {obj.name} at {position}");
         EditorPrefs.SetString(positionKey, positionValue);
 
+        // save the rotation
         string rotationKey = obj.name + "_rotation";
         Quaternion rotation = obj.transform.rotation;
         string rotationValue = rotation.x + "," + rotation.y + "," + rotation.z + "," + rotation.w;
         EditorPrefs.SetString(rotationKey, rotationValue);
+
+        // save the scale
+        string scaleKey = obj.name + "_scale";
+        Vector3 scale = obj.transform.localScale;
+        string scaleValue = scale.x.ToString("F3") + "," + scale.y.ToString("F3") + "," + scale.z.ToString("F3");
+        EditorPrefs.SetString(scaleKey, scaleValue);
     }
-    Debug.Log("PlayMode Changes Saved");
+    
    }
 
    private static void OnPlayModeStateChanged(PlayModeStateChange state){
@@ -46,27 +53,36 @@ public class SavePlayModeChanges
     foreach(GameObject obj in GameObject.FindGameObjectsWithTag("Object")){
         string positionKey = obj.name + "_position";
 
-        // check if the position key exists
+        // check if the position key exists and apply position
         if(EditorPrefs.HasKey(positionKey)){
             string positionValue = EditorPrefs.GetString(positionKey);
-            string[] values = positionValue.Split(',');
-            Vector3 position = new Vector3(float.Parse(values[0]), float.Parse(values[1]), float.Parse(values[2]));
+            string[] positionValues = positionValue.Split(',');
+            Vector3 position = new Vector3(float.Parse(positionValues[0]), float.Parse(positionValues[1]), float.Parse(positionValues[2]));
             obj.transform.position = position;
             Debug.Log($"Applying saved position for object: {obj.name} to {position}");
         }
 
+        // check if the rotation key exists and apply rotation
         string rotationKey = obj.name + "_rotation";
         if(EditorPrefs.HasKey(rotationKey)){
             string rotationValue = EditorPrefs.GetString(rotationKey);
-            string[] values = rotationValue.Split(',');
-            Quaternion rotation = new Quaternion(float.Parse(values[0]), float.Parse(values[1]), float.Parse(values[2]), float.Parse(values[3]));
+            string[] rotationValues = rotationValue.Split(',');
+            Quaternion rotation = new Quaternion(float.Parse(rotationValues[0]), float.Parse(rotationValues[1]), float.Parse(rotationValues[2]), float.Parse(rotationValues[3]));
             obj.transform.rotation = rotation;
             Debug.Log($"Applying saved rotation for object: {obj.name} to {rotation}");
         }
 
-
+        string scaleKey = obj.name + "_scale";
+        if(EditorPrefs.HasKey(scaleKey)){
+            string scaleValue = EditorPrefs.GetString(scaleKey);
+            string[] scaleValues = scaleValue.Split(',');
+            Vector3 scale = new Vector3(float.Parse(scaleValues[0]), float.Parse(scaleValues[1]), float.Parse(scaleValues[2]));
+            obj.transform.localScale = scale;
+            Debug.Log($"Applying saved scale for object: {obj.name} to {scale}");
+        }
         EditorPrefs.DeleteKey(positionKey);
         EditorPrefs.DeleteKey(rotationKey);
+        EditorPrefs.DeleteKey(scaleKey);
     }
     Debug.Log("PlayMode Changes Applied");
    }
